3dd5fb9dfd7bb30199201d3d6c22c8204579b41a
123567sun <292876969@qq.com>
2019-04-17 17:26:37 +0800

    window提交1

--- app/src/main/java/test/sun/com/gogo/BookService.java
+++ app/src/main/java/test/sun/com/gogo/BookService.java
@@ -1,35 +1,24 @@
 package test.sun.com.gogo;
 
 import retrofit2.Call;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 /**
  * @description: Created by sunxiaofei on 2019/2/22.
  */
-public interface MovieService {
+public interface BookService {
     @GET("top250")//https://api.douban.com/v2/movie/
     Call getTop250(@Query("start") int start, @Query("count") int count);
 
-    /**
-     * @deprecated  mac_test 提交修改
-     */
 
 //    @FormUrlEncoded
 //    @POST("top250")
-//    @POST("top250")
-
-
-//    @FormUrlEncoded
-//    @POST("top250")
-
     /**
-     * @deprecated window 提交
+     * @deprecated  windows 修改
+     *
      */
-    //    Call getTop250(@Field("start")int start,@Field("start") int count);
+//    Call getTop250(@Field("start")int start,@Field("start") int count);
 //    Call getTop250(@Field("start")int start,@Field("start") int count);
 
 }
